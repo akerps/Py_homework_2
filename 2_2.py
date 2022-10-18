ca9391afaadd0759d24e1f7dd3224a7d39b31709
Анна <107682652+akerps@users.noreply.github.com>
2022-10-18 14:38:05 +0300

    Update 2_2.py
    исправлено

--- 2_2.py
+++ 2_2.py
@@ -2,10 +2,16 @@
 # Пример:
 # пусть N = 4, тогда (1, 1*2, 1*2*3, 1*2*3*4)
 
+from math import factorial
+
 num = int(input("Введите число: "))
-factorial = 1
-fact_arr= [] 
-for i in range(1, num+1):
-    factorial *= i
-    fact_arr.append(factorial)
+# factorial = 1
+# fact_arr= [] 
+# for i in range(1, num+1):
+#     factorial *= i
+#     fact_arr.append(factorial)
+
+fact_num = lambda x: ((x == 1) and 1) or x * factorial(x -1)
+fact_arr = list(fact_num(i) for i in range(1, num +1))
+
 print(f'N = {num} -> {fact_arr}')
